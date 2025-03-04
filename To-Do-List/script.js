ef0fdfa3949673068eb1cabbb70216ae1ad7c77c
tanmay12 <121058730+tanmay679@users.noreply.github.com>
2025-03-04 11:19:02 +0530

    Add files via upload

---
+++ To-Do-List/script.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; // '×' Close symbol
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("tasks", listContainer.innerHTML);
+}
+
+function showTask() {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+        listContainer.innerHTML = savedTasks;
+    }
+}
+
+showTask();
